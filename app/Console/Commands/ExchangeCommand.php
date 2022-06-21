fac95be5324f2692b453fdfcfea423daddad08a3
tuzlu07x <fatihtuzlu07@icloud.com>
2022-06-21 23:56:31 +0300

    schedule and command done.

---
+++ app/Console/Commands/ExchangeCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Exchange\Bot;
+use App\Models\Exchange;
+use Illuminate\Console\Command;
+
+class ExchangeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'exchange:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $bot = new Bot('https://finans.mynet.com/doviz/');
+
+        foreach ($bot->getAll() as $bot) {
+
+            $exchange = Exchange::updateOrCreate([
+                'name' => $bot['name'],
+                'buy' => $bot['data'][0],
+                'sell' => $bot['data'][1],
+                'differance' => $bot['data'][2],
+                'time' => $bot['data'][3],
+            ]);
+
+            $exchange->save();
+        }
+
+        return 0;
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ExchangeCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,6 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command(ExchangeCommand::class)->everyFiveMinutes();
     }
 
     /**
@@ -25,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
