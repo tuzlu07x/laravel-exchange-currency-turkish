ecf8255c5be1d2013de54aed6270fde9fcdfde17
tuzlu07x <fatihtuzlu07@icloud.com>
2022-06-22 16:05:37 +0300

    fixed.

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -10,6 +10,6 @@ class BotController extends Controller
     public function index()
     {
         $exchanges = Exchange::all();
-        return $exchanges;
+        return json_decode($exchanges);
     }
 }
