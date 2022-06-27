81fd844ebbf5e5a18c62ebda5677dc022a80c6f2
tuzlu07x <fatihtuzlu07@icloud.com>
2022-06-27 16:16:06 +0300

    axios added.

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+import { useState } from 'react';
+import { data } from 'autoprefixer';
 
 function Example() {
+
+    const [name, setName] = useState(null);
+    const [buy, setBuy] = useState(null);
+    const [sell, setSell] = useState(null);
+    const [differance, setDifferance] = useState(null);
+    const [time, setTime] = useState(null);
+    const [errors, setErrors] = useState(null);
+
+    function axiosGet() {
+        axios.get('/', {
+            name: name,
+            buy: buy,
+            sell: sell,
+            differance: differance,
+            time: time,
+        }).then(response => response.data)
+            .then(data => {
+                if (data.success) {
+                    window.location.href = data.redirect;
+                } else {
+                    setErrors(data.errors);
+                }
+
+            }).catch(error => {
+                console.log(error);
+            }
+            );
+    }
+    console.log(name)
     return (
         <div>
             <div className="container mt-5">
@@ -10,34 +42,7 @@ function Example() {
                         <div className='card'>
                             <div className='card-header'>
                                 <div className='card-body'>
-                                    <strong>Dolar</strong>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-md-3'>
-                        <div className='card'>
-                            <div className='card-header'>
-                                <div className='card-body'>
-                                    Deneme
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-md-3'>
-                        <div className='card'>
-                            <div className='card-header'>
-                                <div className='card-body'>
-                                    Deneme
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-md-3'>
-                        <div className='card'>
-                            <div className='card-header'>
-                                <div className='card-body'>
-                                    Deneme
+                                    <strong></strong>
                                 </div>
                             </div>
                         </div>
